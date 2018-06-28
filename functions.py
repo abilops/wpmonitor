9eb7dca4987e5ec03ff0d1e1d42af0320d85f6f1
abilops <abilops@users.noreply.github.com>
2018-06-28 17:21:21 +0530

    Works much better. Tested a bit. Merge-worthy in a couple of days

--- functions.py
+++ functions.py
@@ -1,14 +1,25 @@
 import re
-import datetime
 import time
+import datetime
 import os
 import sys
 import webbrowser
+import urllib
+import webbrowser
+import ssl
+import json
+import requests
+
+from colours import *
 from argsconfig import *
+
+def log(text):
+    if args.quiet == 0 or text.startswith(red):
+        printDate()
+        print(text)
 
 def printDate():
     print(str(datetime.datetime.now()), end='-')
-
 
 def checkEmail(email):
     if email != None:
@@ -34,7 +45,61 @@ def writeFile(path, content):
 def stripStupidDeltas(t):
     if args.delta != None:
         for d in args.delta:
-#        t = re.sub("<div>[\S\s]*.*quotes.*</div>", 'Stupid Quote', t)
             t = re.sub(d, 'Stupid Delta', t)
     return t
 
+# returns HTML text of URL
+def opn(url):
+    try:
+        ctx = ssl.create_default_context()
+        if args.insecure == True:
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+        req = urllib.request.Request(
+            url,
+            headers={'User-Agent': args.useragent})
+        with urllib.request.urlopen(req, context=ctx) as page:
+            pageText = page.read().decode('utf-8')
+        log(lblue + "Page Downloaded")
+        return pageText # <--- Removed str if decode is applied
+    except Exception as e:
+        log(red + "Cannot open URL. Are you connected to the internet? " + str(e) +  white)
+        time.sleep(args.interval/4)
+        return '-1'
+
+# is RESPONSIBLE for returning HTML
+def getHTML(url):
+    pageText = '-1'
+    while pageText == '-1':
+        pageText = opn(url)
+    return stripStupidDeltas(pageText)
+
+def wait(interval):
+    if args.quiet != 0:
+        print("Waiting for {} more seconds...".format(interval), flush=True)
+        time.sleep(interval)
+    else:
+        for x in range(interval, 0, -1):
+            print(clearline + "Waiting for {} more seconds...".format(x), end='', flush=True)
+            time.sleep(1)
+        print(clearline, end='')
+
+def printHeader(interval, fname):
+    if args.quiet != 0:
+        return
+    os.system('/usr/bin/clear')
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print(green + "WEBPAGE MONITOR v1.0" + white)
+    print(green + "Not refreshing any more!!" + white)
+    print("URL: " + lblue + args.url + white)
+    print("Interval: " + lblue + str(interval) + " seconds" + white)
+    print("File: " + lblue + fname + white)
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+def getAttentionAndBrowser(url):
+    if args.headless == False:
+        # Turn on the screen
+        os.system("/usr/bin/xset dpms force on")        
+        os.system("/usr/bin/espeak 'Alert! Webpage has changed.' &")
+        notify('Webpage Monitor', args.url + ' has changed. Opening in new browser tab.\nFingers crossed!')
+        webbrowser.open_new_tab(args.url)
